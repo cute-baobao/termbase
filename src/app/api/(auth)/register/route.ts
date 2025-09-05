00e5543cd236af35d767e5190a4a4264453ea937
郑焕宇 <zhenghuanyu@eslab.cn>
2025-09-05 18:00:41 +0800

    feat: 后端接口添加i18n

--- src/app/api/(auth)/register/route.ts
+++ src/app/api/(auth)/register/route.ts
@@ -1,4 +1,5 @@
-import { responseError } from "@/lib/utils/response";
+import { responseError, responseSuccess } from "@/lib/utils/response";
+import { getTranslations } from "next-intl/server";
 import { NextRequest } from "next/server";
 import z from "zod";
 
@@ -8,11 +9,12 @@ const registerSchema = z.object({
 });
 
 export async function POST(request: NextRequest) {
+  const t = await getTranslations("API.Register");
   const body = await request.json();
   const parsed = registerSchema.safeParse(body);
   if (!parsed.success) {
-    return responseError("缺少参数");
+    return responseError(t("paramsError"), 400);
   }
   console.log(body);
-  return new Response("User registered", { status: 201 });
+  return responseSuccess(null, t("success"), 201);
 }
--- src/lib/utils/response.ts
+++ src/lib/utils/response.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server";
+
 // 通用响应类型
 export interface ApiResponse<T = unknown> {
   success: boolean;
@@ -6,10 +8,23 @@ export interface ApiResponse<T = unknown> {
 }
 
 // 通用响应函数
-export function responseSuccess<T>(data?: T, message = "ok"): ApiResponse<T> {
-  return { success: true, message, data };
+export function responseSuccess<T>(
+  data?: T,
+  message = "ok",
+  status = 200
+): NextResponse {
+  return NextResponse.json(
+    { success: true, message, data },
+    { status }
+  );
 }
 
-export function responseError(message = "error"): ApiResponse<null> {
-  return { success: false, message, data: null };
+export function responseError(
+  message = "error",
+  status = 400
+): NextResponse {
+  return NextResponse.json(
+    { success: false, message, data: null },
+    { status }
+  );
 }
